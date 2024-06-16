915f489fef5f9c47749816f0928dedb940437982
Juro Hinoto <2201150029xo@cyber-u.ac.jp>
2024-06-16 17:40:26 +0900

    cp1
    cpp2

--- routes/notes.js
+++ routes/notes.js
@@ -1,29 +1,37 @@
 var express = require('express');
 var router = express.Router();
-// レスポンスのデータ（ノート全件） 
-const responseObjectDataAll = { 
-    textObject1 : {
-        id: 1,
-        title: 'ノート１のタイトルです',
-        subTitle: 'ノート１のサブタイトルです', bodyText: 'ノート１の本文です'
-    },
-    textObject2 : {
-        id: 2,
-        title: 'ノート２のタイトルです',
-        subTitle: 'ノート２のサブタイトルです', bodyText: 'ノート２の本文です'
-    }
-};
 
-/**
-* メモを全件取得するAPI
-* @returns {Object[]} data
-* @returns {number} data.id - ID 
-* @returns {string} data.title - タイトル 
-* @returns {string} data.text - 内容
-*/
-router.get('/', function (req, res, next) { 
-    // 全件取得して返す 
-    res.json(responseObjectDataAll);
+const { MongoClient } = require("mongodb");
+const uri ="mongodb+srv://2201150029xo:LxnfaHZlrKsKww1w@test.q27siag.mongodb.net/?retryWrites=true&w=majority&appName=Test";
+const client = new MongoClient(uri);
+
+
+
+router.get('/', async (req, res) => {
+    
+    // const { MongoClient } = require("mongodb");
+    // const uri ="mongodb+srv://2201150029xo:LxnfaHZlrKsKww1w@test.q27siag.mongodb.net/?retryWrites=true&w=majority&appName=Test";
+    // const client = new MongoClient(uri);    
+
+    // データベース、コレクションを指定
+    const database = client.db('notes');
+    const notes = database.collection('notes');
+
+    console.log(notes); 
+    
+    //idが1のドキュメントを取得
+    const query ={id: 1};
+
+    console.log('query:'+ query); 
+
+    const note = await notes.findOne(query);
+    
+    console.log(note); 
+
+    res.json(note.title);
+  
 })
+
+
 
 module.exports = router;
